8608418368f3fb19034c8e26dfa449e7efd149df
Tom <tbvanreijn@gmail.com>
2024-01-01 11:39:18 +0100

    logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,6 +14,7 @@ import { Button } from "./ui/button";
 import React from "react";
 import { Database } from "@/types/supabase";
 import ClientSideCredits from "./realtime/ClientSideCredits";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
@@ -28,35 +29,60 @@ export default async function Navbar() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  const {
-    data: credits,
-  } = await supabase.from("credits").select("*").eq("user_id", user?.id ?? '').single()
+  const { data: credits } = await supabase
+    .from("credits")
+    .select("*")
+    .eq("user_id", user?.id ?? "")
+    .single();
 
   return (
     <div className="flex w-full px-4 lg:px-40 py-4 items-center border-b text-center gap-8 justify-between">
       <div className="flex gap-2 h-full">
         <Link href="/">
-          <h2 className="font-bold">Headshots AI</h2>
+          <h2 className=" font-medium  text-2xl items-center  gap-2 flex">
+            <Image
+              src="/logo.png"
+              width={40}
+              height={40}
+              alt="Picture of the author"
+            />{" "}
+            <span className="sm:block hidden"> Sesión de fotos</span>{" "}
+          </h2>
         </Link>
       </div>
       {user && (
         <div className="hidden lg:flex flex-row gap-2">
           <Link href="/overview">
-            <Button variant={"ghost"}>Home</Button>
+            <Button variant={"ghost"}>Inicio</Button>
           </Link>
           {stripeIsConfigured && (
             <Link href="/get-credits">
-              <Button variant={"ghost"}>Get Credits</Button>
+              <Button variant={"ghost"}>Obtener créditos</Button>
             </Link>
           )}
         </div>
       )}
-      <div className="flex gap-4 lg:ml-auto">
+      <div className="flex gap-4 lg:ml-auto scroll-smooth">
         {!user && (
-          <Link href="/login">
-            <Button variant={"ghost"}>Login / Signup</Button>
+          <Link scroll={true} href="/#Como funciona" className="scroll-smooth">
+            <Button variant={"ghost"}>Como funciona?</Button>
           </Link>
         )}
+        {!user && (
+          <Link scroll={true} href="/#Precios" className="scroll-smooth">
+            <Button variant={"ghost"}>Precios</Button>
+          </Link>
+        )}
+        {!user && (
+          <Link scroll={true} href="/#Faq" className="scroll-smooth">
+            <Button variant={"ghost"}>Faq</Button>
+          </Link>
+        )}
+        {!user && (
+          <Link href="/login">
+            <Button variant={"ghost"}>Iniciar sesion / Registrarse</Button>
+          </Link>
+        )}{" "}
         {user && (
           <div className="flex flex-row gap-4 text-center align-middle justify-center">
             {stripeIsConfigured && (
@@ -67,15 +93,16 @@ export default async function Navbar() {
                 <AvatarIcon height={24} width={24} className="text-primary" />
               </DropdownMenuTrigger>
               <DropdownMenuContent className="w-56">
-                <DropdownMenuLabel className="text-primary text-center overflow-hidden text-ellipsis">{user.email}</DropdownMenuLabel>
+                <DropdownMenuLabel className="text-primary text-center overflow-hidden text-ellipsis">
+                  {user.email}
+                </DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <form action="/auth/sign-out" method="post">
                   <Button
                     type="submit"
                     className="w-full text-left"
-                    variant={"ghost"}
-                    >
-                    Log out
+                    variant={"ghost"}>
+                    Cerrar sesión
                   </Button>
                 </form>
               </DropdownMenuContent>
