2636c914de773cbd062aad55546f2da5bc535b78
Tom <tbvanreijn@gmail.com>
2024-01-01 12:45:57 +0100

    pricetable

--- components/stripe/StripeTable.tsx
+++ components/stripe/StripeTable.tsx
@@ -1,27 +1,31 @@
-'use client'
-import { User } from '@supabase/supabase-js';
-import React, { useEffect } from 'react';
+"use client";
+import { User } from "@supabase/supabase-js";
+import React, { useEffect } from "react";
 
-interface StripePricingTableProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
-  'pricing-table-id': string;
-  'publishable-key': string;
+interface StripePricingTableProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLElement>,
+    HTMLElement
+  > {
+  "pricing-table-id": string;
+  "publishable-key": string;
 }
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      'stripe-pricing-table': StripePricingTableProps;
+      "stripe-pricing-table": StripePricingTableProps;
     }
   }
 }
 
 type Props = {
   user: User;
-}
+};
 
 const StripePricingTable = ({ user }: Props) => {
   useEffect(() => {
-    const script = document.createElement('script');
+    const script = document.createElement("script");
     script.src = "https://js.stripe.com/v3/pricing-table.js";
     script.async = true;
 
@@ -29,20 +33,17 @@ const StripePricingTable = ({ user }: Props) => {
 
     return () => {
       document.body.removeChild(script);
-    }
+    };
   }, []);
 
   return (
-    <div className='flex flex-1 flex-col w-full'>
-      <stripe-pricing-table 
-        pricing-table-id="prctbl_1NtEZSCMCmxSLOnr1kGCuIRl" 
-        publishable-key="pk_live_51MS6I1CMCmxSLOnrhl3L60fqOQGXesUyz67QnTKMXBsspDEjifIaldAdrPZ5rAfg6A0VCEdNCoLwzwfnfKpD48bO00pvYzF8yG" 
-        client-reference-id={user.id}
-        customer-email={user.email}
-        >
-      </stripe-pricing-table>
+    <div className="flex flex-1 flex-col w-full">
+      <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
+      <stripe-pricing-table
+        pricing-table-id="prctbl_1OAA6NFHQxgFOGeCPGcZvSEG"
+        publishable-key="pk_live_51O5QeBFHQxgFOGeCAf2jRzTU8BdbX8Ec3AwOZMeJFW89IlrzHuks9iFlvITh3ZArfZnofo8kxf0tBOU9kI02aGrV00cvwBdBYb"></stripe-pricing-table>
     </div>
   );
-}
+};
 
 export default StripePricingTable;
