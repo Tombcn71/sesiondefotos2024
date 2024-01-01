c11b81d42c9eb4e2af083c69328ee92a0c7157c0
Tom <tbvanreijn@gmail.com>
2024-01-01 15:39:50 +0100

    color

--- components/realtime/ClientSideModelsList.tsx
+++ components/realtime/ClientSideModelsList.tsx
@@ -74,7 +74,7 @@ export default function ClientSideModelsList({
           <h1 className="text-2xl">Comience entrenando su primer modelo. </h1>
           <div>
             <Link href="/overview/models/train">
-              <Button className="sky-blue-600">Entrenar modelo</Button>
+              <Button className="bg-sky-600 lg">Entrenar modelo</Button>
             </Link>
           </div>
         </div>
