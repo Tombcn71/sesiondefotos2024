31532fd35c585efa019202c1eb288dd3c8ff215c
Tom <tbvanreijn@gmail.com>
2024-01-17 18:38:57 +0100

    nu dan

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default async function Index() {
       <div className="flex flex-col lg:flex-row items-center gap-8 p-8 max-w-6xl w-full">
         <div className="flex flex-col space-y-4 lg:w-1/2 w-full">
           <h1 className="text-5xl font-bold">
-            newest Sesión de fotos profesionales con IA en minutos.{" "}
+            nu dan Sesión de fotos profesionales con IA en minutos.{" "}
           </h1>
           <p className="text-gray-600 text-lg">
             Eleva tu presencia en línea con fotografías en alta definición
